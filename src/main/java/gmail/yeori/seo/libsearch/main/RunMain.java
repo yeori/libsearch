438dd3fafe429091c0ff71a5101f5fbb9d42c34e
YeoriSeo <yeori.seo@gmail.com>
2014-12-16 15:53:58 +0900

    init 시작.

---
+++ src/main/java/gmail/yeori/seo/libsearch/main/RunMain.java
@@ -0,0 +1,13 @@
+package gmail.yeori.seo.libsearch.main;
+
+public class RunMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
