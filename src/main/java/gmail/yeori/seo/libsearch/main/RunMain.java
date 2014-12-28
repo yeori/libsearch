bf14f6b0e187d93da2fd8aae9d5a37ecd33aafb0
YeoriSeo <yeori.seo@gmail.com>
2014-12-29 03:46:04 +0900

    Library Engine 구현 나중에 없어질거 같은데...?

--- src/main/java/gmail/yeori/seo/libsearch/main/RunMain.java
+++ src/main/java/gmail/yeori/seo/libsearch/main/RunMain.java
@@ -7,7 +7,23 @@ public class RunMain {
 	 */
 	public static void main(String[] args) {
 		System.out.println("test run");
+		
+		LibraryEngine engine = new LibraryEngine(
+				"http://www.eplib.or.kr",
+				"/service/search.asp",
+				"POST");
+//		engine.setUrl("");
 
+	}
+	
+	public static class LibraryEngine {
+		private String host;
+		private String formUrl;
+		private String method;
+		public LibraryEngine(String host, String formUrl, String method) {
+			// TODO Auto-generated constructor stub
+		}
+		
 	}
 
 }
