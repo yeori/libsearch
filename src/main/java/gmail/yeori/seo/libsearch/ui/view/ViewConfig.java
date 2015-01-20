a4f90f6795f00021687821039796c3fb01a45b42
YeoriSeo <yeori.seo@gmail.com>
2015-01-20 16:29:33 +0900

    필터를 ViewConfig 에서 관리하고 있음.

--- src/main/java/gmail/yeori/seo/libsearch/ui/view/ViewConfig.java
+++ src/main/java/gmail/yeori/seo/libsearch/ui/view/ViewConfig.java
@@ -1,14 +1,44 @@
 package gmail.yeori.seo.libsearch.ui.view;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import gmail.yeori.seo.libsearch.engine.IFilter;
 import gmail.yeori.seo.libsearch.ui.IPageRequestAction;
 
 public class ViewConfig {
 	private IPageRequestAction actionHandler;
+	private List<IFilter> filters = new ArrayList<>();
+	private Map<String, IView<?>> views = new HashMap<String, IView<?>>();
+	
 	public void setPageActionHandler(IPageRequestAction actionHandler) {
 		this.actionHandler = actionHandler;
 	}
 	public IPageRequestAction getPageActionHandler() {
 		return this.actionHandler;
+	}
+	
+	public void addFilter ( IFilter filter) {
+		
+		if ( filter != null ){
+			this.filters.add(filter);			
+		}
+	}
+	
+	public void clearFilters() {
+		this.filters.clear();
+	}
+	
+	public List<IFilter> getFilters() {
+		return this.filters;
+	}
+	public void registerView(String name, IView<?> view) {
+		views.put(name, view);
+	}
+	public IView getView(String name) {
+		return views.get(name);
 	}
 
 }
