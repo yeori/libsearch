1f9fb0500405961216715a5c6997e9c035f639d9
YeoriSeo <yeori.seo@gmail.com>
2015-01-20 16:33:24 +0900

    또다시 필터가 메소드 시그니처로 추가됨.

--- src/main/java/gmail/yeori/seo/libsearch/ui/IPageRequestAction.java
+++ src/main/java/gmail/yeori/seo/libsearch/ui/IPageRequestAction.java
@@ -1,5 +1,9 @@
 package gmail.yeori.seo.libsearch.ui;
 
+import gmail.yeori.seo.libsearch.engine.IFilter;
+
+import java.util.List;
+
 public interface IPageRequestAction {
-	public void pageRequested ( String keyword, int pageIndex );
+	public void pageRequested ( String keyword, int pageIndex, List<IFilter> filters );
 }
